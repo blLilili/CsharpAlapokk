7da3efc6e30a06062c002a322e8e49e070c79c61
blLilili <biboklilivbj@gmail.com>
2024-09-06 09:16:01 +0200

    "huh"

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CsharpAlapokk;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        System.Console.WriteLine("Az első argumentum: "+ args[0]);
+        
+        Console.Write("1. sor;");
+        System.Console.WriteLine("2. sor;");
+    }
+}
